f9e278dbe35b2bcbc1e7df6af75da6aa374f7f3f
nitinsinghchouhan21 <141238389+nitinsinghchouhan21@users.noreply.github.com>
2025-10-13 17:07:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { RTCPeerConnection, nonstandard } = require('wrtc');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+ 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
